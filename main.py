340a27c171f4fb28a5fe54063e651736563c0662
Ovchinnikov Danila <danovch24@gmail.com>
2024-11-02 18:24:21 +0100

    я пытался

--- main.py
+++ main.py
@@ -1,14 +1,26 @@
+
 def fix_trailing_whitespace(line):
     return line.rstrip()
 
-def fix_code(filename):
-    with open(filename, 'r') as f:
+
+def fix_indentation(line):
+    leading_spaces = len(line) - len(line.lstrip())
+    corrected_indent = ' ' * ((leading_spaces + 3) // 4 * 4)
+    return corrected_indent + line.lstrip()
+
+
+def fix_code(file_name):
+    with open(file_name, 'r') as f:
         lines = f.readlines()
     fixed_lines = []
     for line in lines:
         line = fix_trailing_whitespace(line)
+        line = fix_indentation(line)
         fixed_lines.append(line)
     return "\n".join(fixed_lines)
+
+
+
 
 
 if __name__ == "__main__":
