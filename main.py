e6185af17ebce79268641b7e2657f5edf9704786
Ovchinnikov Danila <danovch24@gmail.com>
2024-11-02 17:44:44 +0100

    привет, катя 2

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("Привет катя")
+def fix_trailing_whitespace(line):
+    return line.rstrip()
+
+def fix_code(filename):
+    with open(filename, 'r') as f:
+        lines = f.readlines()
+    fixed_lines = []
+    for line in lines:
+        line = fix_trailing_whitespace(line)
+        fixed_lines.append(line)
+    return "\n".join(fixed_lines)
+
+
+if __name__ == "__main__":
+    filename = "example.txt"
+    fixed_code = fix_code(filename)
+    print(fixed_code)
